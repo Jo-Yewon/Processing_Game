ef1009b6a5eaf584dfb99588afaa77a06a2ae774
Jo Yewon <yeawonjo@gmail.com>
2019-05-14 14:36:04 +0900

    Add files via upload

---
+++ arduino_sound.ino
@@ -0,0 +1,13 @@
+const int SOUND_SENSOR_PIN = A0;
+int sensorValue;
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(SOUND_SENSOR_PIN, INPUT);
+}
+ 
+void loop() {
+  sensorValue = analogRead(SOUND_SENSOR_PIN);
+  Serial.println(sensorValue);
+  delay(20);
+}
